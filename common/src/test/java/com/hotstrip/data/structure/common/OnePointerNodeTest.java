b03b58cc664cb968bf41b8016975636a9c3b7fa9
Eleven <idiotalex@163.com>
2021-02-20 22:13:16 +0800

    add node test class

--- common/src/test/java/com/hotstrip/data/structure/common/OnePointerNodeTest.java
+++ common/src/test/java/com/hotstrip/data/structure/common/OnePointerNodeTest.java
@@ -1,13 +1,18 @@
 package com.hotstrip.data.structure.common;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.Test;
 
 @Slf4j
 public class OnePointerNodeTest {
-    public static void main(String[] args) {
+
+    @Test
+    public void test1() {
         OnePointerNode node = new OnePointerNode();
         node.setData(1);
 
         log.info("node data: [{}]", node.getData());
+        Assert.assertEquals(1, node.getData());
     }
 }
