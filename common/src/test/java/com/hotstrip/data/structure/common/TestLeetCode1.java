8fa59b8e344fb3d3d5e9e43683595d7b807cb386
Eleven <idiotalex@163.com>
2021-08-20 00:10:38 +0800

    update leetcode week one test class

--- common/src/test/java/com/hotstrip/data/structure/common/TestLeetCode1.java
+++ common/src/test/java/com/hotstrip/data/structure/common/TestLeetCode1.java
@@ -8,6 +8,61 @@ import org.junit.Test;
 public class TestLeetCode1 {
 
     /**
+     * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+     *
+     * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+     *
+     * 你可以按任意顺序返回答案。
+     *
+     *  
+     *
+     * 示例 1：
+     *
+     * 输入：nums = [2,7,11,15], target = 9
+     * 输出：[0,1]
+     * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+     * 示例 2：
+     *
+     * 输入：nums = [3,2,4], target = 6
+     * 输出：[1,2]
+     * 示例 3：
+     *
+     * 输入：nums = [3,3], target = 6
+     * 输出：[0,1]
+     *  
+     *
+     * 提示：
+     *
+     * 2 <= nums.length <= 104
+     * -109 <= nums[i] <= 109
+     * -109 <= target <= 109
+     * 只会存在一个有效答案
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/two-sum
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    @Test
+    public void testTwoSum() {
+        log.info("{}", twoSum(new int[] {2, 7, 11, 15}, 9));
+        log.info("{}", twoSum(new int[] {3, 2, 4}, 6));
+        log.info("{}", twoSum(new int[] {3, 3}, 6));
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        // 双重遍历数组，判断两个数的和是否等于 target
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+
+
+    /**
      * 判断是否是回文数，从左往右读取和从右往左读取数字结果相同
      * 121 true
      * -121 false
@@ -303,18 +358,19 @@ public class TestLeetCode1 {
      * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
      */
     @Test
-    public void test() {
+    public void testReverseWords() {
         Assert.assertEquals("blue is sky the", reverseWords("the sky is blue"));
-        Assert.assertEquals("world! hello", reverseWords("  hello world!  "));
-        Assert.assertEquals("example good a", reverseWords("a good   example"));
+        Assert.assertEquals("world! hello", reverseWords("  hello world!  "));
+        Assert.assertEquals("example good a", reverseWords("a good   example"));
     }
 
     public String reverseWords(String s) {
         StringBuilder builder = new StringBuilder();
+        // 去掉首尾空格
+        s = s.trim();
         // 先根据空格分割字符串，然后忽略为空的子字符串，最后倒序拼接
-        String[] strings = s.split(" ");
+        String[] strings = s.split("\\s+");
         for (int i = strings.length - 1; i >= 0; i--) {
-            log.info("[{}]", strings[i]);
             // 忽略为空的子字符串
             if (!" ".equals(strings[i])) {
                 builder.append(strings[i]);
