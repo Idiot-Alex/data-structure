9995a34705fde50c623b793afd0ca16822840c74
Eleven <idiotalex@163.com>
2021-08-19 20:13:06 +0800

    add leetcode week one test class

---
+++ common/src/test/java/com/hotstrip/data/structure/common/TestLeetCode1.java
@@ -0,0 +1,357 @@
+package com.hotstrip.data.structure.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.Test;
+
+@Slf4j
+public class TestLeetCode1 {
+
+    /**
+     * 判断是否是回文数，从左往右读取和从右往左读取数字结果相同
+     * 121 true
+     * -121 false
+     * 999 true
+     * 86568 true
+     */
+    @Test
+    public void testIsPalindrome() {
+        boolean flag = isPalindrome(8);
+        log.info("result is: {}", flag);
+        Assert.assertEquals(true, flag);
+
+        flag = isPalindrome(100);
+        log.info("result is: {}", flag);
+        Assert.assertEquals(false, flag);
+
+        flag = isPalindrome(-80);
+        log.info("result is: {}", flag);
+        Assert.assertEquals(false, flag);
+
+        flag = isPalindrome(818);
+        log.info("result is: {}", flag);
+        Assert.assertEquals(true, flag);
+    }
+
+    public boolean isPalindrome(int x) {
+        // 负数不会是回文数
+        if (x < 0) {
+            return false;
+        }
+        // 把数字转换成字符串数组，遍历首尾对应的下标，判断是否相等
+        char[] array = String.valueOf(x).toCharArray();
+        for (int i = 0; i < array.length / 2; i++) {
+            if (array[i] != array[array.length - 1 - i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 给你一个字符串 s，由若干单词组成，单词前后用一些空格字符隔开。返回字符串中最后一个单词的长度。
+     *
+     * 单词 是指仅由字母组成、不包含任何空格字符的最大子字符串。
+     *
+     *  
+     *
+     * 示例 1：
+     *
+     * 输入：s = "Hello World"
+     * 输出：5
+     * 示例 2：
+     *
+     * 输入：s = "   fly me   to   the moon  "
+     * 输出：4
+     * 示例 3：
+     *
+     * 输入：s = "luffy is still joyboy"
+     * 输出：6
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/length-of-last-word
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    @Test
+    public void testLengthOfLastWord() {
+        log.info("{}", lengthOfLastWord("Hello World"));
+        log.info("{}", lengthOfLastWord("   fly me   to   the moon  "));
+    }
+    public int lengthOfLastWord(String s) {
+        int count = 0;
+        boolean flag = false;
+        // 转换成字符数组，从后往前遍历，找到第一个不为 ' ' 的下标，累加到下一个为 ' '为止
+        char[] chars = s.toCharArray();
+        for (int i = chars.length - 1; i >= 0; i--) {
+            if (chars[i] != ' ') {
+                count++;
+                flag = true;
+            }
+            if (flag && chars[i] == ' ') {
+                break;
+            }
+        }
+        // 返回长度
+        return count;
+    }
+
+    /**
+     * 给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+     *
+     * 说明：本题中，我们将空字符串定义为有效的回文串。
+     *
+     *  
+     *
+     * 示例 1:
+     *
+     * 输入: "A man, a plan, a canal: Panama"
+     * 输出: true
+     * 解释："amanaplanacanalpanama" 是回文串
+     * 示例 2:
+     *
+     * 输入: "race a car"
+     * 输出: false
+     * 解释："raceacar" 不是回文串
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/valid-palindrome
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * ASCII 码表
+     * a-z：97-122
+     * A-Z：65-90
+     * 0-9：48-57
+     */
+    @Test
+    public void testIsPalindromeStr() {
+        Assert.assertEquals(false, isPalindrome(""));
+        Assert.assertEquals(true, isPalindrome("A man, a plan, a canal: Panama"));
+        Assert.assertEquals(false, isPalindrome("race a car"));
+    }
+
+    public boolean isPalindrome(String s) {
+        if (s.length() == 0) {
+            return false;
+        }
+        // 先去掉非数字和字母的字符，用 ASCII 码
+        StringBuilder builder = new StringBuilder("");
+        for (char c : s.toCharArray()) {
+            if ((48 <= c && c <= 57) || (65 <= c && c <= 90) || (97 <= c && c <= 122)) {
+                builder.append(c);
+            }
+        }
+        log.info("{}", builder.toString());
+        // 然后判断是否是回文字符串，字符数组，遍历首尾对应的下标，判断是否相等(需要注意忽略大小写)
+        char[] array = builder.toString().toLowerCase().toCharArray();
+        for (int i = 0; i < array.length / 2; i++) {
+            if (array[i] != array[array.length - 1 - i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
+     *
+     * 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+     *
+     * 你可以假设数组中的所有字符都是 ASCII 码表中的可打印字符。
+     *
+     *  
+     *
+     * 示例 1：
+     *
+     * 输入：["h","e","l","l","o"]
+     * 输出：["o","l","l","e","h"]
+     * 示例 2：
+     *
+     * 输入：["H","a","n","n","a","h"]
+     * 输出：["h","a","n","n","a","H"]
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/reverse-string
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    @Test
+    public void testReverseString() {
+        char[] chars = new char[] {'h', 'e', 'l', 'l', 'o'};
+        reverseString(chars);
+        log.info("{}", chars);
+
+        chars = new char[] {'h', 'a', 'n', 'n', 'a', 'h'};
+        reverseString(chars);
+        log.info("{}", chars);
+    }
+
+    public void reverseString(char[] s) {
+        char temp;
+        // 遍历数组，交换首尾相对应的字符，用临时变量存储
+        for (int i = 0; i <s.length / 2 ; i++) {
+            temp = s[i];
+            s[i] = s[s.length - 1 -i];
+            s[s.length - 1 -i] = temp;
+        }
+    }
+
+    /**
+     * 给你一个有效的 IPv4 地址 address，返回这个 IP 地址的无效化版本。
+     *
+     * 所谓无效化 IP 地址，其实就是用 "[.]" 代替了每个 "."。
+     *
+     *  
+     *
+     * 示例 1：
+     *
+     * 输入：address = "1.1.1.1"
+     * 输出："1[.]1[.]1[.]1"
+     * 示例 2：
+     *
+     * 输入：address = "255.100.50.0"
+     * 输出："255[.]100[.]50[.]0"
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/defanging-an-ip-address
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    @Test
+    public void testDefangIPaddr() {
+        Assert.assertEquals("1[.]1[.]1[.]1", defangIPaddr("1.1.1.1"));
+        Assert.assertEquals("255[.]100[.]50[.]0", defangIPaddr("255.100.50.0"));
+    }
+
+    public String defangIPaddr(String address) {
+        // 字符串转换成字符数组，遍历，检测到 . 就替换成 [.]
+        StringBuilder builder = new StringBuilder();
+        for (char c : address.toCharArray()) {
+            if (c == '.') {
+                builder.append("[.]");
+            } else {
+                builder.append(c);
+            }
+        }
+        return builder.toString();
+    }
+
+    /**
+     * 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+     *
+     *  
+     *
+     * 示例 1：
+     *
+     * 输入：s = "We are happy."
+     * 输出："We%20are%20happy."
+     *  
+     *
+     * 限制：
+     *
+     * 0 <= s 的长度 <= 10000
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    @Test
+    public void testReplaceSpace() {
+        Assert.assertEquals("We%20are%20happy.", replaceSpace("We are happy."));
+    }
+
+    public String replaceSpace(String s) {
+        // 遍历字符数组，匹配空格，然后替换字符串
+        StringBuilder builder = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (c == ' ') {
+                builder.append("%20");
+            } else {
+                builder.append(c);
+            }
+        }
+        return builder.toString();
+    }
+
+    /**
+     * 输入一个英文句子，翻转句子中单词的顺序，但单词内字符的顺序不变。为简单起见，标点符号和普通字母一样处理。例如输入字符串"I am a student. "，则输出"student. a am I"。
+     *
+     *  
+     *
+     * 示例 1：
+     *
+     * 输入: "the sky is blue"
+     * 输出: "blue is sky the"
+     * 示例 2：
+     *
+     * 输入: "  hello world!  "
+     * 输出: "world! hello"
+     * 解释: 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+     * 示例 3：
+     *
+     * 输入: "a good   example"
+     * 输出: "example good a"
+     * 解释: 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+     *  
+     *
+     * 说明：
+     *
+     * 无空格字符构成一个单词。
+     * 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+     * 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+     * 注
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/fan-zhuan-dan-ci-shun-xu-lcof
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    @Test
+    public void test() {
+        Assert.assertEquals("blue is sky the", reverseWords("the sky is blue"));
+        Assert.assertEquals("world! hello", reverseWords("  hello world!  "));
+        Assert.assertEquals("example good a", reverseWords("a good   example"));
+    }
+
+    public String reverseWords(String s) {
+        StringBuilder builder = new StringBuilder();
+        // 先根据空格分割字符串，然后忽略为空的子字符串，最后倒序拼接
+        String[] strings = s.split(" ");
+        for (int i = strings.length - 1; i >= 0; i--) {
+            log.info("[{}]", strings[i]);
+            // 忽略为空的子字符串
+            if (!" ".equals(strings[i])) {
+                builder.append(strings[i]);
+                // 如果不是最后一个，就添加空格
+                if (i > 0) {
+                    builder.append(" ");
+                }
+            }
+        }
+        return builder.toString();
+    }
+
+    /**
+     * 写一个函数 StrToInt，实现把字符串转换成整数这个功能。不能使用 atoi 或者其他类似的库函数。
+     *
+     *  
+     *
+     * 首先，该函数会根据需要丢弃无用的开头空格字符，直到寻找到第一个非空格的字符为止。
+     *
+     * 当我们寻找到的第一个非空字符为正或者负号时，则将该符号与之后面尽可能多的连续数字组合起来，作为该整数的正负号；假如第一个非空字符是数字，则直接将其与之后连续的数字字符组合起来，形成整数。
+     *
+     * 该字符串除了有效的整数部分之后也可能会存在多余的字符，这些字符可以被忽略，它们对于函数不应该造成影响。
+     *
+     * 注意：假如该字符串中的第一个非空格字符不是一个有效整数字符、字符串为空或字符串仅包含空白字符时，则你的函数不需要进行转换。
+     *
+     * 在任何情况下，若函数不能进行有效的转换时，请返回 0。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/ba-zi-fu-chuan-zhuan-huan-cheng-zheng-shu-lcof
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    @Test
+    public void testStrToInt() {
+
+    }
+
+    public int strToInt(String str) {
+        return 0;
+    }
+}
