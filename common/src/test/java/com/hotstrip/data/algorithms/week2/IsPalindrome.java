5f1ac9effe31da8f7f7e68e5f17607f2866f9ce0
Eleven <idiotalex@163.com>
2021-08-29 18:42:42 +0800

    add leetcode week2 test class

--- common/src/test/java/com/hotstrip/data/algorithms/week2/IsPalindrome.java
+++ common/src/test/java/com/hotstrip/data/algorithms/week2/IsPalindrome.java
@@ -39,10 +39,58 @@ public class IsPalindrome {
 
     @Test
     public void test() {
-
+        ListNode l1 = ListNode.initListNode(new int[] {1, 1, 2, 1});
+        log.info(".......................{}",
+                isPalindrome(l1));
     }
 
     public boolean isPalindrome(ListNode head) {
-        return false;
+        // 使用快慢指针找到链表的中间节点
+        ListNode midNode = getMidNode(head);
+        // 然后反转中间节点之后的链表
+        ListNode newNode = reverse(midNode.next);
+        // 最后判断反转之后的链表和前半部分链表是否相等
+        ListNode p = head;
+        ListNode q = newNode;
+        while (q != null) {
+            if (p.val != q.val)
+                return false;
+            p = p.next;
+            q = q.next;
+        }
+        return true;
+    }
+
+    /**
+     * 获取中间节点
+     * @param head
+     * @return
+     */
+    private ListNode getMidNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    /**
+     * 反转后面部分的节点
+     * @param head
+     * @return
+     */
+    private ListNode reverse(ListNode head) {
+        ListNode header = new ListNode();
+        ListNode p = head;
+
+        while (p != null) {
+            ListNode temp = p.next;
+            p.next = header.next;
+            header.next = p;
+            p = temp;
+        }
+        return header.next;
     }
 }
